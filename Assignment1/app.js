32f76290a649aad0b3bd86a0c1779a2f8bee64df
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-01 18:06:11 -0500

    WIP: Bugfix

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,30 +1,52 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("Server Started!!!!!!!");
     const url = req.url;
     const requestMethod = req.method;
     if (url === "/") {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Welcome</title></head>');
         res.write('<h1>WELCOME TO MY HOUSE!!!</h1>');
-        res.write('<body><form action="/create-user" method="POST">Username: <input type="text"><br><br><button>Send</button></form></body>');
+        res.write('<body><form action="/create-user" method="POST">Username: <input type="text"><br><br><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.statusCode = 302;
-        res.setHeader('Location','/create-user');
-        res.end();
+        return res.end();
     }
     if (url === "/users") {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>List of Users</title></head>');
-        res.write('<h1>Give it up for all our guests : </h1>');
-        res.write('<ul><li>Ross</li><li>Rachel</li><li>Monica</li><li>Phoebe</li><li>Joey</li><li>Chandler</li></ul>');
+        res.write('<body><h1>Give it up for all our guests : </h1>');
+        res.write('<ul><li>Ross</li><li>Rachel</li><li>Monica</li><li>Phoebe</li><li>Joey</li><li>Chandler</li></ul></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === "/create-user" && requestMethod === "POST") {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log('parsedBody', parsedBody);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    /*if (url === '/create-user') {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         res.end();
-    }
-    if (url === "/create-user" && requestMethod === "POST"){
-        console.log(req.data.toString());
-    }
+    }*/
 });
 
-server.listen(3001);
+server.listen(3003);
