6215031cdeb739695f54a0ba6e30435e7491a3c7
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-01 15:43:21 -0500

    add git ignore

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,3 +1,10 @@
 const http = require('http');
 
-http.createServer();
+const server = http.createServer((req, res) => {
+    console.log("Server Started!!!!!!!");
+    const url = req.url;
+    console.log();
+
+});
+
+server.listen(3000);
