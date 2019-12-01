0c7b058da157af565fede03af679a0433b81cb02
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-01 16:25:39 -0500

    redirect to create-user page from default page

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -3,23 +3,28 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log("Server Started!!!!!!!");
     const url = req.url;
-    if (url == "/") {
+    const requestMethod = req.method;
+    if (url === "/") {
         res.write('<html>');
         res.write('<head><title>Welcome</title></head>');
         res.write('<h1>WELCOME TO MY HOUSE!!!</h1>');
-        res.write('<body><form action="/create-user" method="post">Username: <input type="text"><br><button>Send</button></form></body>');
+        res.write('<body><form action="/create-user" method="POST">Username: <input type="text"><br><br><button>Send</button></form></body>');
         res.write('</html>');
+        res.statusCode = 302;
+        res.setHeader('Location','/create-user');
         res.end();
-    } else if (url == "/users") {
+    }
+    if (url === "/users") {
         res.write('<html>');
         res.write('<head><title>List of Users</title></head>');
         res.write('<h1>Give it up for all our guests : </h1>');
         res.write('<ul><li>Ross</li><li>Rachel</li><li>Monica</li><li>Phoebe</li><li>Joey</li><li>Chandler</li></ul>');
         res.write('</html>');
         res.end();
-    } else if (url == "/create-user"){
+    }
+    if (url === "/create-user" && requestMethod === "POST"){
         console.log(req.data.toString());
     }
 });
 
-server.listen(3000);
+server.listen(3001);
