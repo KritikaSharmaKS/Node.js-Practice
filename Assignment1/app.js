f738c678c74d68acf5c3ba3866bbff730091aa6b
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-01 15:50:35 -0500

    display welcome message on default route

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -3,7 +3,15 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log("Server Started!!!!!!!");
     const url = req.url;
-    console.log();
+    if (url == "/") {
+        res.write('<html>');
+        res.write('<head><title>Welcome</title></head>');
+        res.write('<h1>WELCOME TO MY HOUSE!!!</h1>');
+        res.write('</html>');
+    } else if (url == "/users") {
+
+    }
+    console.log(url);
 
 });
 
