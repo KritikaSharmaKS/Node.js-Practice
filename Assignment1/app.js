bed9483dd746186a8070c0c42026abd6ea1dbd2e
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-01 16:03:36 -0500

    add users list to users route + add form to default route

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -7,12 +7,17 @@ const server = http.createServer((req, res) => {
         res.write('<html>');
         res.write('<head><title>Welcome</title></head>');
         res.write('<h1>WELCOME TO MY HOUSE!!!</h1>');
+        res.write('<form method="post">Username: <input type="text"><br><button>Send</button></form>');
         res.write('</html>');
+        res.end();
     } else if (url == "/users") {
-
+        res.write('<html>');
+        res.write('<head><title>List of Users</title></head>');
+        res.write('<h1>Give it up for all our guests : </h1>');
+        res.write('<ul><li>Ross</li><li>Rachel</li><li>Monica</li><li>Phoebe</li><li>Joey</li><li>Chandler</li></ul>');
+        res.write('</html>');
+        res.end();
     }
-    console.log(url);
-
 });
 
 server.listen(3000);
