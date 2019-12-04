026cff5eae0c6511e5e947a888061b5dd011419e
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-04 16:51:27 -0500

    sending a response is easy now

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     console.log("In the 2nd Middleware!");
     //... return a response if next is not called
+    res.send('<h1>Hello from Express!!!</h1>'); //Header already set
 });
 
 const server = http.createServer(app);
