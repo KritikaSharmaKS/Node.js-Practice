e04687f82d6b65b66e2303114d890e4b192a6d16
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-04 15:52:50 -0500

    create a new main server file

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+
+console.log("Starting Main Server.............");
+
+const server = http.createServer((req, res) => {
+    console.log("Hey, I am in the Server!!!");
+});
+
+server.listen(3001);
