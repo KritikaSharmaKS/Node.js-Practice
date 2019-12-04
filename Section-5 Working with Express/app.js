d0799886ac19de32b43ea27aa0a33f9230869984
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-04 16:46:08 -0500

    use of the next function in parsing the request using middlewares

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log("In the Middleware!");
+    next(); //allows the request to go thru the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log("In the 2nd Middleware!");
+    //... return a response if next is not called
 });
 
 const server = http.createServer(app);
