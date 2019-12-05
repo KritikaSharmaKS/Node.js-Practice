473d7f3598aa54f2ff8d3a46250cb40735c8d7bd
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-05 09:09:20 -0500

    practice handle routes

--- Section-5 Working with Express/Assignment2.js
+++ Section-5 Working with Express/Assignment2.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Assignment 2 First Middleware');
-    next();
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Users Page!!!</h1>');
 });
 
-app.use((req, res, next) => {
-    res.send('<h1>Assignment 2 using Express JS</h1>');
+app.use('/', (req, res, next) => {
+    res.send('<h1>Default Page!!!</h1>');
 });
+
+// app.use((req, res, next) => {
+//     console.log('Assignment 2 First Middleware');
+//     next();
+// });
+//
+// app.use((req, res, next) => {
+//     res.send('<h1>Assignment 2 using Express JS</h1>');
+// });
 
 app.listen(3006);
