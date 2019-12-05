039474259a176606596ff44404a5e698bdeddf9e
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-05 08:54:00 -0500

    handling routes with express

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the Middleware!");
-    next(); //allows the request to go thru the next middleware
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log("In the 1st Middleware!");
+    res.send('<h1>Add Product Page!!!</h1>')
+    //next(); //allows the request to go thru the next middleware
+});
+
+app.use('/', (req, res, next) => {
     console.log("In the 2nd Middleware!");
     //... return a response if next is not called
     res.send('<h1>Hello from Express!!!</h1>'); //Header already set
