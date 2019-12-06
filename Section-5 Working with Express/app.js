fc0be4df96f26aca4dbb3cdda1f9049901a2b86b
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-06 09:54:55 -0500

    parsing incoming requests

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
+app.use(bodyParser.urlencoded({msExtendedCode: false}));
+
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs!');
+//     next();
+// });
 
 app.use('/add-product', (req, res, next) => {
-    console.log("In the 1st Middleware!");
-    res.send('<h1>Add Product Page!!!</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"></button></form>')
     //next(); //allows the request to go thru the next middleware
 });
 
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-    console.log("In the 2nd Middleware!");
     //... return a response if next is not called
     res.send('<h1>Hello from Express!!!</h1>'); //Header already set
 });
