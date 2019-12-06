fd8f3bf49d1c767a8154fb901bd895cc9b548c64
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-06 13:58:47 -0500

    add a 404 Error Page

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -6,7 +6,7 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({msExtendedCode: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // app.use('/', (req, res, next) => {
 //     console.log('This always runs!');
@@ -14,7 +14,11 @@ app.use(bodyParser.urlencoded({msExtendedCode: false}));
 // });
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+
 
 app.listen(3005);
