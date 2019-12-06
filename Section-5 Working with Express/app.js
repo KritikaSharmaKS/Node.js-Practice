f384b7e7fcd73c505a8c145a5c342c02d2515c7b
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-06 10:52:52 -0500

    adjust code according to new express router

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -10,15 +10,7 @@ app.use(bodyParser.urlencoded({msExtendedCode: false}));
 //     next();
 // });
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><button type="submit">SEND</button></form>')
-    //next(); //allows the request to go thru the next middleware
-});
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
 
 app.use('/', (req, res, next) => {
     //... return a response if next is not called
