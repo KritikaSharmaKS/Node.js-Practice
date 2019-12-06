c0b999396fab90333ee6c11b0db8d399dca84306
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-06 14:54:56 -0500

    create a page not found error html page

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -17,7 +18,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
