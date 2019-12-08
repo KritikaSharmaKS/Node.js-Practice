58527ecaa32aa595f88b86bb78816f072f93c895
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-07 19:45:50 -0500

    use a helper function for navigation

--- Section-5 Working with Express/routes/admin.js
+++ Section-5 Working with Express/routes/admin.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 const router = express.Router();
 
+const rootDirName = require('../util/path');
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDirName, 'views', 'add-product.html'));
     //next(); //allows the request to go thru the next middleware
 });
 
