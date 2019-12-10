8f759400f49e83be79ec111d351c1b46451be984
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-12-10 13:49:23 -0500

    render html pages to the 2 routes specified

--- Section-5 Assignment 3/index.js
+++ Section-5 Assignment 3/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 app.use('/users', (req, res, next) => {
-
+    res.sendFile(path.join(__dirname, 'views', 'users.html'));
 });
 
 app.use((req, res, next) => {
-
+    res.sendFile(path.join(__dirname, 'views', 'default.html'));
 });
-
 
 app.listen(3006);
