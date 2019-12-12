c2698e3e594f1cf9462d0c91bed27a5fe9b72f0f
Kritika Sharma <kritika.sharma@sinopetech.com>
2019-12-12 10:31:17 -0500

    work with dynamic information entered by user

--- Section-5 Working with Express/routes/shop.js
+++ Section-5 Working with Express/routes/shop.js
@@ -5,7 +5,10 @@ const rootDirName = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
+   console.log("shop.js" , adminData.products);
    res.sendFile(path.join(rootDirName, 'views', 'shop.html'));
 });
 
