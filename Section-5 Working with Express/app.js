2798d3c90b26ef782b36a07fdaacfd2e94b99c83
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-02-03 15:46:45 -0500

    use pug

--- Section-5 Working with Express/app.js
+++ Section-5 Working with Express/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const path = require('path');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
