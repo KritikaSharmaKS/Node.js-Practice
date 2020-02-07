d2f3cd01b213906aa725a65d4cb971a41adb3205
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-02-07 10:18:37 -0500

    send an object to the pug view to render dynamic content on the UUI

--- Section-5 Working with Express/routes/admin.js
+++ Section-5 Working with Express/routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const productArray = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDirName, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path: '/add-product'});
     //next(); //allows the request to go thru the next middleware
 });
 
