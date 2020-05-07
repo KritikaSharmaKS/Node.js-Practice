a288366435c54e1a75fcc4b732d07fcca7c0dcf3
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-05-06 22:26:21 -0400

    create the main file and define routes

---
+++ Section-6 Assignment 4/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const defaultRoute = require('./routes/default');
+const usersRoute = require('./routes/users');
+
+const app = express();
+
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRoute);
+app.use(defaultRoute);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: "404", path:''});
+});
+
+app.listen(3007);
