f4e7d3b01ec6ae90ea07279038596a26056d4e55
KritikaSharmaKS <sh.kritika01@gmail.com>
2020-05-06 22:47:20 -0400

    add the users route file

---
+++ Section-6 Assignment 4/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const usernameArray = require('./default').usernameArray;
+
+router.get('/users', (req, res, next) => {
+    res.render('users', {pageTitle: 'Users', path: '/users', usernameArray });
+});
+
+exports.routes = router;
